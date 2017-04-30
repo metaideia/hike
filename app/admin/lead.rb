262debe3bc739f1fe8a540d8aab066c8aaa5d4f7
Robson Marques <robsonmarques@gmail.com>
2017-04-30 07:48:40 -0300

    Adiciona filtro para status nos leads

--- app/admin/lead.rb
+++ app/admin/lead.rb
@@ -2,6 +2,7 @@ ActiveAdmin.register Lead do
   menu priority: 3
 
   filter :lists
+  filter :subscriptions_status_eq, as: :select, collection: SubscriptionStatus.to_a, label: "Status"
   filter :name
   filter :email
   filter :created_at
